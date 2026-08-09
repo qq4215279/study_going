c1c185343629dacb86ae219692be020a11e9eb66
liuzhen <liu865488158@qq.com>
2026-08-09 15:21:34 +0800

    delete day0729-elasticsearch

--- day0729-elasticsearch/src/main/java/com/demo/elasticsearch/repository/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.demo.elasticsearch.repository;
-
-import com.demo.elasticsearch.pojo.Item;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface ItemRepository extends ElasticsearchRepository<Item,Long> {
-}
